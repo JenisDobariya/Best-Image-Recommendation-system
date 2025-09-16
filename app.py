8d21e9e7b08ab7b5fb2621f6f7344b743dcf645c
Jenish0001 <jenishdobariya2003@gmail.com>
2025-09-16 11:02:35 +0400

    main

--- app.py
+++ app.py
@@ -221,5 +221,5 @@ if __name__ == "__main__":
 
     print("Open your browser:http://localhost:5000")
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
-    # app.run(debug=False)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False)
